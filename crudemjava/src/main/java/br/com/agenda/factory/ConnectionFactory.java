a9f5cbd89ead542663e9133e32b3e83224011700
Luan Tobias <luantmd50@gmail.com>
2023-11-09 22:02:42 -0300

    First steps

---
+++ crudemjava/src/main/java/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package br.com.agenda.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    public static final String DATABASE_URL = "jdbc:mysql//localhost:3306/agenda";
+    
+    public static Connection createConnectionToMySQL () throws Exception {
+        Class.forName ("com.mysql.jdbc.Driver");
+        
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        
+        return connection;
+}
+    
+    public static void main (String [] args) throws Exception {
+    	Connection con = createConnectionToMySQL (); //Call method
+    	if (con!=null){ // "if" avoid to connect several times
+    	System.out.println ("Connection with success!");
+    	con.close ();
+    	}
+    }
+
+}
